c89f8ee6b707fd6877902f770b2006f1727877ce
Ahmet Çadırcı <ahmetcadirci25@gmail.com>
2018-08-12 14:00:03 +0300

    base url revize

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-APP.get('/manifest.json', function(req, res) {
+APP.get('/ana-ekrana-ekle/manifest.json', function(req, res) {
     res.append('Content-Type', 'text/javascript');
     res.sendFile(path.join(root, "manifest.json"));
 });
 
-APP.get('/sw.js', function(req, res) {
+APP.get('/ana-ekrana-ekle/sw.js', function(req, res) {
     res.append('Content-Type', 'text/javascript');
     res.sendFile(path.join(root, "sw.js"));
 });
